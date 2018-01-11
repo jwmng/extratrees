ff71f7e395e7038d10425221b80984f40fe1c6de
jwmng <mail@jwmng.nl>
2018-01-11 10:53:40 +0100

    Make benchmark pylint compliant

--- docs/benchmark.py
+++ docs/benchmark.py
@@ -1,18 +1,27 @@
 import time
 import sys
-from src.extratrees import Dataset, ExtraForest
-
-""" 
-MNIST CSV set from:
-https://pjreddie.com/media/files/mnist_train.csv
-"""
-
 from pathlib import Path
 
-train_file = Path('./docs/mnist/mnist_train.csv')
-test_file = Path('./docs/mnist/mnist_test.csv')
+from src.extratrees import Dataset, ExtraForest
+
+
+TRAIN_FILE = Path('./docs/mnist/mnist_train.csv')
+TEST_FILE = Path('./docs/mnist/mnist_test.csv')
+
 
 def load_data(file_, n_points=None):
+    """
+    Load CSV data from `file` and return the first `n` points as Dataset
+
+    The CSV file should have the first column as labels and the rest as data.
+    All data must be integer.
+    Works well with the MNIST set from:
+    https://pjreddie.com/media/files/mnist_train.csv
+
+    Args:
+        file_ (pathlib.Path): File to load
+        n_points (int): Number of datapoints to load. Loads all if not `None`.
+    """
     try:
         lines = file_.read_text().splitlines()[:n_points]
     except FileNotFoundError:
@@ -25,7 +34,7 @@ def load_data(file_, n_points=None):
     attributes = []
     n_lines = len(lines)
     for idx, line in enumerate(lines):
-        if idx % 10000 == 0:
+        if idx % (n_lines/10) == 0:
             print("%5d/%d" % (idx, n_lines))
         split_ = line.split(',')
         labels.append(int(split_[0]))
@@ -34,40 +43,56 @@ def load_data(file_, n_points=None):
 
 
 def score(classifier, test_sample):
+    """ Accuracy on test set.  """
     predictions = classifier.predict(test_sample.attributes)
     correct = [pred == true
                for pred, true in zip(predictions, test_sample.outputs)]
 
     return sum(correct) / len(correct)
 
-def bench(n_training, n_trees):
+
+def bench(train_data, test_data, n_trees):
+    """
+    Benchmark Extratrees
+
+    Trains a ExtraForest with `n_trees` trees and returns training time,
+    evaluation time and testing accuracy.
+
+    Args:
+        train_data (extratrees.Datset): Train set
+        test_data (extratrees.Datset): Test set
+        n_trees (int): Number of trees
+    """
+
     print('-'*20)
     print("Going to train %d trees" % n_trees)
     forest = ExtraForest(n_trees=n_trees, n_min=10)
-    print("Loading training data")
-    train_set = load_data(train_file, n_points=n_training)
 
     print("Training...")
-    t0 = time.time()
-    forest.fit(train_set)
-    t_train = time.time() - t0
+    t_start = time.time()
+    forest.fit(train_data)
+    t_train = time.time() - t_start
     print("Trained in %.3fs" % t_train)
 
-    print("Loading test data")
-    test_set = load_data(test_file)
     print("Testing")
-    t0 = time.time()
-    test_score = score(forest, test_set)
-    t_test = time.time() - t0
+    t_start = time.time()
+    test_score = score(forest, test_data)
+    t_test = time.time() - t_start
     print("Evaluated in %.3fs" % t_test)
     print("Score: %.3f" % test_score)
 
+
 if __name__ == '__main__':
     try:
-        n_trees = int(sys.argv[2])
-        n_training = int(sys.argv[1])
+        N_TREES = int(sys.argv[2])
+        N_TRAINING = int(sys.argv[1])
     except (IndexError, ValueError):
         print("Usage: benchmark_mnist.py <n_training> <n_trees>")
         sys.exit(1)
 
-    bench(n_training, n_trees)
+    print("Loading training data")
+    TRAIN_SET = load_data(TRAIN_FILE, n_points=N_TRAINING)
+
+    print("Loading test data")
+    TEST_SET = load_data(TEST_FILE)
+    bench(TRAIN_SET, TEST_SET, N_TREES)
